f76cef41153d581515a96b103cba85f4b1186d98
Ananth Sethuraman <ananth_sethuraman@yahoo.com>
2020-06-11 13:44:03 -0700

    Add files via upload

--- santanu.py
+++ santanu.py
@@ -1,6 +1,20 @@
 #-----------------------------------------------------------------------
 
+print ("")
+print ("STATUS:: Begin of santanu.py")
+
+#-----------------------------------------------------------------------
+
+import importlib
+import inspect
+import math
+import sys
 import tensorflow as tf
+import time
+
+#-----------------------------------------------------------------------
+
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
 #-----------------------------------------------------------------------
 
@@ -12,9 +26,9 @@ NOT_SPECIFIED_2 = -1
 # The input images are 28 pixels x 28 pixels.
 # All pixels are black and white.
 
-NUM_ROW_IN_IMG = 28
-NUM_COL_IN_IMG = 28
-NUM_CHANNEL_IN_IMG = 1
+NUM_ROW_INP_IMG = 28
+NUM_COL_INP_IMG = 28
+NUM_CHANNEL_INP_IMG = 1
 
 # The first convolutional layer's kernel.
 # The convolution will transform each input image into an image with
@@ -25,7 +39,7 @@ NUM_CHANNEL_IN_IMG = 1
 NUM_ROW_KERNEL_CONV_LAYER_1 = 5
 NUM_COL_KERNEL_CONV_LAYER_1 = 5
 NUM_CHANNEL_CONV_LAYER_1 = 64
-PADDING_CONV_LAYER_1 = 'SAME'
+PADDING_CONV_LAYER_1 = "SAME"
 
 # Max pooling decreases number of pixels along x-axis by a factor of 2 and
 # along y-axis by a factor of 2.
@@ -35,6 +49,7 @@ PADDING_CONV_LAYER_1 = 'SAME'
 # the image after pooling 1, is 14 x 14 pixels with 64 channels
 
 PIXEL_DECREASE_MAX_POOL_LAYER_1 = 2
+PADDING_MAX_POOL_LAYER_1 = "SAME"
 
 # The second convolutional layer's kernel
 # As an example let the image coming into this layer has 14 x 14 pixels
@@ -44,7 +59,7 @@ PIXEL_DECREASE_MAX_POOL_LAYER_1 = 2
 NUM_ROW_KERNEL_CONV_LAYER_2 = 5
 NUM_COL_KERNEL_CONV_LAYER_2 = 5
 NUM_CHANNEL_CONV_LAYER_2 = 128
-PADDING_CONV_LAYER_2 = 'SAME'
+PADDING_CONV_LAYER_2 = "SAME"
 
 # Max pooling decreases number of pixels along x-axis by a factor of 2 and
 # along y-axis by a factor of 2.
@@ -55,19 +70,22 @@ PADDING_CONV_LAYER_2 = 'SAME'
 # 128 channels.
 
 PIXEL_DECREASE_MAX_POOL_LAYER_2 = 2
+PADDING_MAX_POOL_LAYER_2 = "SAME"
 
 # Fully connected layer
 # Eg if the image coming in is 7 x 7 pixels with 128 channels, it is
 # treated as a column vector with 7 * 7 * 128 = 6272 entries = 6272
 # neurons.
 
-NUM_IN_NEURON_FC_LAYER = NUM_ROW_IN_IMG
-NUM_IN_NEURON_FC_LAYER /= PIXEL_DECREASE_MAX_POOL_LAYER_1
-NUM_IN_NEURON_FC_LAYER /= PIXEL_DECREASE_MAX_POOL_LAYER_2
+NUM_INP_NEURON_FC_LAYER = NUM_ROW_INP_IMG
+NUM_INP_NEURON_FC_LAYER /= PIXEL_DECREASE_MAX_POOL_LAYER_1
+NUM_INP_NEURON_FC_LAYER /= PIXEL_DECREASE_MAX_POOL_LAYER_2
 
-NUM_IN_NEURON_FC_LAYER *= NUM_COL_IN_IMG
-NUM_IN_NEURON_FC_LAYER /= PIXEL_DECREASE_MAX_POOL_LAYER_1
-NUM_IN_NEURON_FC_LAYER /= PIXEL_DECREASE_MAX_POOL_LAYER_2
+NUM_INP_NEURON_FC_LAYER *= NUM_COL_INP_IMG
+NUM_INP_NEURON_FC_LAYER /= PIXEL_DECREASE_MAX_POOL_LAYER_1
+NUM_INP_NEURON_FC_LAYER /= PIXEL_DECREASE_MAX_POOL_LAYER_2
+
+NUM_INP_NEURON_FC_LAYER = int (NUM_INP_NEURON_FC_LAYER)
 
 NUM_OUT_NEURON_FC_LAYER = 1024
 
@@ -79,78 +97,79 @@ NUM_CLASS = 10
 
 NUM_EPOCH = 20
 BATCH_SIZE = 256
-DROPOUT = 0.75
+DROPOUT_KEEP_PROB = 0.75
+LEARNING_RATE = 0.01
 
 #-----------------------------------------------------------------------
 
 # Placeholders
 
-tmp = NUM_ROW_IN_IMG * NUM_COL_IN_IMG * NUM_CHANNEL_IN_IMG
-raw_x = tf.placeholder (tf.float32,
-                        [NOT_SPECIFIED_1, tmp])
+tmp = NUM_ROW_INP_IMG * NUM_COL_INP_IMG * NUM_CHANNEL_INP_IMG
+raw_x = tf.compat.v1.placeholder (tf.float32,
+                                  [NOT_SPECIFIED_1, tmp])
 x = tf.reshape (raw_x, shape = [NOT_SPECIFIED_2,
-                                NUM_ROW_IN_IMG,
-                                NUM_COL_IN_IMG,
-                                NUM_CHANNEL_IN_IMG])
+                                NUM_ROW_INP_IMG,
+                                NUM_COL_INP_IMG,
+                                NUM_CHANNEL_INP_IMG])
 
-y = tf.placeholder (tf.float32, [NOT_SPECIFIED_1, 10])
+y = tf.compat.v1.placeholder (tf.float32, [NOT_SPECIFIED_1, 10])
 
-keep_prob = tf.placelholder (tf.float32)
+keep_prob = tf.compat.v1.placeholder (tf.float32)
 
 #-----------------------------------------------------------------------
 
 # Coefficients of the convolutional kernel in convolutional layer 1
 
-W_conv1 = tf.Variable (tf.random_normal ([NUM_ROW_KERNEL_CONV_LAYER_1,
+W_conv1 = tf.Variable (tf.random.normal ([NUM_ROW_KERNEL_CONV_LAYER_1,
                                           NUM_COL_KERNEL_CONV_LAYER_1,
-                                          NUM_CHANNEL_IN_IMG,
+                                          NUM_CHANNEL_INP_IMG,
                                           NUM_CHANNEL_CONV_LAYER_1]))
 
 # Bias just after first convolution
 
-B_conv1 = tf.Variable (tf.random_normal ([NUM_CHANNEL_CONV_LAYER_1])
+B_conv1 = tf.Variable (tf.random.normal ([NUM_CHANNEL_CONV_LAYER_1]))
 
 # Coefficients of the convolutional kernel in convolutional layer 2
 
-W_conv2 = tf.Variable (tf.random_normal ([NUM_ROW_KERNEL_CONV_LAYER_2,
+W_conv2 = tf.Variable (tf.random.normal ([NUM_ROW_KERNEL_CONV_LAYER_2,
                                           NUM_COL_KERNEL_CONV_LAYER_2,
                                           NUM_CHANNEL_CONV_LAYER_1,
                                           NUM_CHANNEL_CONV_LAYER_2]))
 
 # Bias just after second convolution
 
-B_conv2 = tf.Variable (tf.random_normal ([NUM_CHANNEL_CONV_LAYER_2])
+B_conv2 = tf.Variable (tf.random.normal ([NUM_CHANNEL_CONV_LAYER_2]))
 
 # Weights of the full-connected layer
 
-W_fc = tf.Variable (tf.random_normal ([NUM_IN_NEURON_FC_LAYER,
+W_fc = tf.Variable (tf.random.normal ([NUM_INP_NEURON_FC_LAYER,
                                        NUM_OUT_NEURON_FC_LAYER]))
 
 # Bias of the fully-connected layer
 
-B_fc = tf.Variable (tf.random_normal ([NUM_OUT_NEURON_FC_LAYER]))
+B_fc = tf.Variable (tf.random.normal ([NUM_OUT_NEURON_FC_LAYER]))
 
 # Weights of the output layer
 
-W_out = tf.Variable (tf.random_normal ([NUM_OUT_NEURON_FC_LAYER,
+W_out = tf.Variable (tf.random.normal ([NUM_OUT_NEURON_FC_LAYER,
                                         NUM_CLASS]))
 
 # Bias of the output layer
 
-B_out = tf.Variable (tf.random_normal ([NUM_CLASS]))
+B_out = tf.Variable (tf.random.normal ([NUM_CLASS]))
 
 #-----------------------------------------------------------------------
 
 def cnn (x,
          w_conv1, w_conv2, w_fc, w_out,
          b_conv1, b_conv2, b_fc, b_out,
-         dropout):
+         dropout_rate):
 
     # First convolutional layer
 
     conv1 = tf.nn.conv2d (x, w_conv1,
                           strides = [1, 1, 1, 1],
-                          padding = 'PADDING_CONV_LAYER_1')
+                          padding = PADDING_CONV_LAYER_1)
 
     # Add a bias to the first convolutional layer
 
@@ -162,18 +181,18 @@ def cnn (x,
 
     # First max pooling layer
 
-    pool1 = tf.nn.max_pool (relu1,
-                            ksize   = [1, PIXEL_DECREASE_MAX_POOL_LAYER_1,
-                                       PIXEL_DECREASE_MAX_POOL_LAYER_1, 1],
-                            strides = [1, PIXEL_DECREASE_MAX_POOL_LAYER_1,
-                                       PIXEL_DECREASE_MAX_POOL_LAYER_1, 1],
-                            padding = 'PADDING_MAX_POOL_LAYER_1')
+    pool1 = tf.nn.max_pool2d (relu1,
+                              ksize   = [1, PIXEL_DECREASE_MAX_POOL_LAYER_1,
+                                         PIXEL_DECREASE_MAX_POOL_LAYER_1, 1],
+                              strides = [1, PIXEL_DECREASE_MAX_POOL_LAYER_1,
+                                         PIXEL_DECREASE_MAX_POOL_LAYER_1, 1],
+                              padding = PADDING_MAX_POOL_LAYER_1)
 
     # Second convolutional layer
 
-    conv2 = tf.nn.conv2d (x, w_conv2,
+    conv2 = tf.nn.conv2d (pool1, w_conv2,
                           strides = [1, 1, 1, 1],
-                          padding = 'PADDING_CONV_LAYER_2')
+                          padding = PADDING_CONV_LAYER_2)
 
     # Add a bias
 
@@ -185,20 +204,20 @@ def cnn (x,
 
     # Max pooling
 
-    pool2 = tf.nn.max_pool (relu2,
+    pool2 = tf.nn.max_pool2d (relu2,
                             ksize   = [1, PIXEL_DECREASE_MAX_POOL_LAYER_2,
                                        PIXEL_DECREASE_MAX_POOL_LAYER_2, 1],
                             strides = [1, PIXEL_DECREASE_MAX_POOL_LAYER_2,
                                        PIXEL_DECREASE_MAX_POOL_LAYER_2, 1],
-                            padding = 'PADDING_MAX_POOL_LAYER_2')
+                            padding = PADDING_MAX_POOL_LAYER_2)
 
     # Fully Connected Layer
 
-    fc = tf.reshape (conv2, [NOT_SPECIFIED_2, NUM_IN_NEURON_FC_LAYER])
+    fc = tf.reshape (pool2, [NOT_SPECIFIED_2, NUM_INP_NEURON_FC_LAYER])
     fc = tf.matmul (fc, w_fc)
     fc = tf.add (fc, b_fc)
     fc = tf.nn.relu (fc)
-    fc = tf.nn.dropout (fc, keep_prob)
+    fc = tf.nn.dropout (fc, rate = dropout_rate)
 
     # Output layer
 
@@ -214,7 +233,7 @@ def cnn (x,
 y_pred = cnn (x,
               W_conv1, W_conv2, W_fc, W_out,
               B_conv1, B_conv2, B_fc, B_out,
-              keep_prob)
+              1.0 - keep_prob)
 
 # Objective function
 
@@ -225,7 +244,7 @@ ace_node = avg_cross_entropy
 
 # Algorithm to minimize the objective function
 
-algo = tf.train.AdamOptimizer (learning_rate = LEARNING_RATE)
+algo = tf.compat.v1.train.AdamOptimizer (learning_rate = LEARNING_RATE)
 algo = algo.minimize (ace_node)
 algo_node = algo
 
@@ -241,17 +260,15 @@ al2e_node = avg_l2_err
 
 # Initialize
 
-init_node = tf.global_variables_initializer ()
+init_node = tf.compat.v1.global_variables_initializer ()
 
 #-----------------------------------------------------------------------
 
 # Load the MNIST data. We use the notation "mds" for MNIST data set
 
-print (f'STATUS:: Begin reading MNIST data set')
-mds =
-    tf.examples.tutorials.mnist.input_data.read_data_sets ("MNIST_data/",
-                                                           one_hot = True)
-print (f'STATUS:: End reading MNIST data set')
+print ("STATUS:: Begin reading MNIST data set")
+mds = read_data_sets ("MNIST_data/", one_hot = True)
+print ("STATUS:: End reading MNIST data set")
 
 #-----------------------------------------------------------------------
 
@@ -260,43 +277,52 @@ print (f'STATUS:: End reading MNIST data set')
 start_time = time.time ()
 
 num_batch = mds.train.num_examples / BATCH_SIZE
-print ('fSTATUS:: Number of batches = {num_batch:3}')
+num_batch = int (math.ceil (num_batch))
+print ("STATUS:: Number of batches = ", num_batch)
 
 with tf.Session () as sess:
-    print (f'STATUS:: Begin initializing computational graph')
+    print ("STATUS:: Begin initializing computational graph")
     sess.run (init_node)
-    print (f'STATUS:: End initializing computational graph')
+    print ("STATUS:: End initializing computational graph")
 
-    print (f'STATUS:: Begin of training')
-    print (f'STATUS:: Number of epochs during training = {NUM_EPOCH}\n')
+    print ("STATUS:: Begin of training")
+    print ("STATUS:: Number of epochs during training = ", NUM_EPOCH, "\n")
 
     for i in range (NUM_EPOCH):
-        print (f'STATUS:: Begin of epoch {i:3}')
+        print ("STATUS:: Begin of epoch ", i)
 
         for j in range (num_batch):
-            x_bat, y_bat = mds.train.next_batch (mds)
+            x_bat, y_bat = mds.train.next_batch (BATCH_SIZE)
             sess.run (algo_node, feed_dict = {x         : x_bat,
                                               y         : y_bat,
-                                              keep_prob : DROPOUT})
+                                              keep_prob : DROPOUT_KEEP_PROB})
             ace_bat, al2e_bat = sess.run ([ace_node, al2e_node],
                                            feed_dict = {x         : x_bat,
                                                         y         : y_bat,
                                                         keep_prob : 1}) 
 
-        print (f'STATUS:: At the end of epoch {i:3} Average cross-entropy error = {ace_bat}    Average L2 error = {al2e_bat}')
-        print (f'STATUS:: End of epoch {i:3}\n')
+        print ("STATUS:: ",
+               "End of epoch ", i, "    ",
+               "Average cross-entropy error = ", ace_bat, "    ",
+               "Average L2 error = ", al2e_bat)
 
-    print (f'STATUS:: End of training')
+    print ("STATUS:: End of training")
 
     # Testing phase
 
     al2e_test = sess.run (al2e_node, feed_dict = {x         : mds.test.images[:256],
                                                   y         : mds.test.labels[:256],
                                                   keep_prob : 1})
-    print (f'STATUS:: Average L2 error in the testing phase = {al2e_test}')
+    print ("STATUS:: Average L2 error in the testing phase = ", al2e_test)
 
 end_time = time.time ()
-print (f'STATUS:: Total processing time = {end_time - start_time}')
+print ("STATUS:: Total processing time = ", end_time - start_time)
+
+#-----------------------------------------------------------------------
+
+print ("")
+print ("STATUS:: End of santanu.py")
+print ("")
 
 #-----------------------------------------------------------------------
 
